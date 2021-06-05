c9db4eb2c509e2f9e0f833c11b10eda870d9f11f
Mikhail Bulash <scaryspiderpig@gmail.com>
2021-06-05 14:29:06 +0200

    Review vscode extensions

--- .config/vscode/configure.sh
+++ .config/vscode/configure.sh
@@ -17,7 +17,9 @@ for t in settings.json keybindings.json snippets; do
     ln -sfn "${thisdir}/${t}" "${confdir}/${t}"
 done
 
-comm -13 <(code --list-extensions | sort -u) <(sort -u "${thisdir}/extensions.txt") \
+comm -13 \
+    <(code --list-extensions | sort -u) \
+    <(grep -E -v "^(#|$)" "${thisdir}/extensions.txt" | sort -u) \
 | while read -r extid; do
-    code --install-extension "${extid}"
+    code --install-extension "${extid}" --force
 done
