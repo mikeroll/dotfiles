fd115c57d9bdb6441264bae8a07bd47a0e203793
Mikhail Bulash <scaryspiderpig@gmail.com>
2021-06-06 16:09:48 +0200

    Power management tweaks

--- .config/aconfmgr/20-platform.sh
+++ .config/aconfmgr/20-platform.sh
@@ -12,8 +12,14 @@ AddPackage lm_sensors # Collection of user space tools for general SMBus access 
 CopyFile /etc/conf.d/lm_sensors
 CreateLink /etc/systemd/system/multi-user.target.wants/lm_sensors.service /usr/lib/systemd/system/lm_sensors.service
 
+# Power management
+AddPackage powertop
+CopyFile /etc/modprobe.d/bluetooth.conf
+CopyFile /etc/modprobe.d/i915.conf
+CopyFile /etc/modprobe.d/iwlwifi.conf
+CopyFile /etc/sysctl.d/nmi_watchdog.conf
+CopyFile /etc/udev/rules.d/90-runtime-pm.rules
 CopyFile /etc/sysctl.d/10-vm.conf
 
-AddPackage powertop
-
+# Device tweaks
 CopyFile /etc/udev/hwdb.d/99-laptop-keyboard.hwdb
